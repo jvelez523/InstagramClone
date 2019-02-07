26bcc76e10301b4dbf100306fb5667628ffa81ea
Justin Velez <justin.velez0523@gmail.com>
2019-02-07 10:27:08 -0500

    Used GraphQL to create server back-end

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
     return <div className="App">
             <Header />
             <section className="App-main">
-              <Post nickname="Justin" avatar="https://media.licdn.com/dms/image/C4D03AQGZtqP74_XMgw/profile-displayphoto-shrink_100_100/0?e=1554940800&v=beta&t=2d5KXvlVVr9NIT7jH35zH3EKx5KJARIHTIGs6Ro-eLo" caption="Moving the community!" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
-              <Post nickname="OG" avatar="https://media.licdn.com/dms/image/C4D03AQGZtqP74_XMgw/profile-displayphoto-shrink_100_100/0?e=1554940800&v=beta&t=2d5KXvlVVr9NIT7jH35zH3EKx5KJARIHTIGs6Ro-eLo" caption="Holding a mic" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
+              <Post nickname="Justin" avatar="https://media.licdn.com/dms/image/C4D03AQGZtqP74_XMgw/profile-displayphoto-shrink_100_100/0?e=1554940800&v=beta&t=2d5KXvlVVr9NIT7jH35zH3EKx5KJARIHTIGs6Ro-eLo" caption="That's a mighty fine beach!" image="https://s.thestreet.com/files/tsc/v2008/photos/contrib/uploads/ace23929-4f1e-11e8-a5fd-018c7d910674_600x400.jpg" />
+              <Post nickname="Blark" avatar="https://media.licdn.com/dms/image/C4E03AQEtBed4fkxp6A/profile-displayphoto-shrink_200_200/0?e=1551312000&v=beta&t=r0737AKI8UWzVuHmLwSUcaUbEMxbmotwJeYSrm_NkDI" caption="Holding a mic" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
 
               {/* more posts */}
             </section>
