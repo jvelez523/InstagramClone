75cbd3597f8796b2a26ab732abd42cfc7131ce30
jvelez523 <justin.velez0523@gmail.com>
2019-02-06 23:05:07 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header'
+import Post from './components/Post'
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+    return <div className="App">
+            <Header />
+            <section className="App-main">
+              <Post nickname="Justin" avatar="https://media.licdn.com/dms/image/C4D03AQGZtqP74_XMgw/profile-displayphoto-shrink_100_100/0?e=1554940800&v=beta&t=2d5KXvlVVr9NIT7jH35zH3EKx5KJARIHTIGs6Ro-eLo" caption="Moving the community!" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
+              <Post nickname="OG" avatar="https://media.licdn.com/dms/image/C4D03AQGZtqP74_XMgw/profile-displayphoto-shrink_100_100/0?e=1554940800&v=beta&t=2d5KXvlVVr9NIT7jH35zH3EKx5KJARIHTIGs6Ro-eLo" caption="Holding a mic" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
 
-export default App;
+              {/* more posts */}
+            </section>
+          </div>;
+      }
+    }
+
+    export default App;
